3b262166e61b8b069dc216d137a487ac4b568d64
sonya <sonp2401@gmail.com>
2021-04-29 22:08:18 +0300

    174A - Задача на равенство

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,30 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n=in.nextInt(),myMax=0,myS=0,myMin=0;
+        int n=in.nextInt(),b=in.nextInt();
+        double sum=0;
+        sum+=b;
+        int[] arr=new int[n];
+
         for (int i = 0; i <n ; i++) {
-            int a=in.nextInt();
-            if (a>myMax){
-                myS=myMax;
-                myMax=a;
+            arr[i]=in.nextInt();
+            sum+=arr[i];
+        }
+        boolean f=true;
+        double sum2=0;
+        for (int i = 0; i <n; i++) {
+            if (arr[i]>(sum/n)) {
+                f=false;
+                break;
             }
-            else if (a<myMax&&a>myS){
-                myMin=myS;
-                myS=a;
-            }
-            else if (a<myMax&&a<myS&&a>myMin){
-                myMin=a;
+            sum2+=(sum/n)-arr[i];
+        }
+
+        if (!f||(int)sum2*1000000>(int)b*1000000) System.out.println(-1);
+        else {
+            for (int i = 0; i <n ; i++) {
+                System.out.printf("%.7f\n", (sum/n)-arr[i]);
             }
         }
-        if (myMax!=0&&myMin!=0&&myS!=0)
-        System.out.println(myMax+" "+myS+" "+myMin);
-        else System.out.println("-1 -1 -1");
     }
 }
