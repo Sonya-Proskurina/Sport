3466287792a732bc21d0da9c315637fb39234b2d
sonya <sonp2401@gmail.com>
2021-04-29 17:23:12 +0300

    A. Идеально неидеальный массив #716

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,28 @@ package com.company;
 import java.util.*;
 
 public class Main {
+    static boolean antiSqrt(int n) {
+        boolean b = true;
+        for (int i = 0; i <= n / 2; i++) {
+            if (i * i == n) b = false;
+        }
+        if (n == 1) b = false;
+        return b;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt(),n,k;
-        for (int i = 0; i <t ; i++) {
-            n=in.nextInt();
-            int[] arr=new int[n];
-            k=in.nextInt();
-
-            for (int j = 0; j <n ; j++) {
-                 arr[j]=in.nextInt();
-            }
-            int s=0;
-            for (int j = 0; j <k; j++) {
-                if (arr[s]!=0&&s!=n-1) {
-                    arr[s]--;
-                    arr[n-1]++;
+        int t = in.nextInt(), n, k;
+        for (int i = 0; i < t; i++) {
+            n = in.nextInt();
+            k = 0;
+            for (int j = 0; j < n; j++) {
+                if (antiSqrt(in.nextInt())) {
+                    k++;
                 }
-                else if (s!=n-1) {
-                    s++;
-                    j--;
-                }
-                else break;
             }
-            for (int j = 0; j <n ; j++) {
-                System.out.print(arr[j]+" ");
-            }
+            if (k == 0) System.out.println("NO");
+            else System.out.println("YES");
         }
     }
 }
