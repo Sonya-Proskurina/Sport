e699e56ce2c287bddc23a4dfebe2abbc3902ba72
sonya <sonp2401@gmail.com>
2021-04-29 16:56:28 +0300

    A. Око за око #717

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,18 +5,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        for (int i = 0; i < num; i++) {
-            long ostatok, k = 0;
-            long n = in.nextLong();
-            if (n < 2050 || n % 2050 != 0) System.out.println(-1);
-            else {
-                ostatok = n / 2050;
-                while (ostatok > 0) {
-                    k += ostatok % 10;
-                    ostatok /= 10;
+        int t=in.nextInt(),n,k;
+        for (int i = 0; i <t ; i++) {
+            n=in.nextInt();
+            int[] arr=new int[n];
+            k=in.nextInt();
+
+            for (int j = 0; j <n ; j++) {
+                 arr[j]=in.nextInt();
+            }
+            int s=0;
+            for (int j = 0; j <k; j++) {
+                if (arr[s]!=0&&s!=n-1) {
+                    arr[s]--;
+                    arr[n-1]++;
                 }
-                System.out.println(k);
+                else if (s!=n-1) {
+                    s++;
+                    j--;
+                }
+                else break;
+            }
+            for (int j = 0; j <n ; j++) {
+                System.out.print(arr[j]+" ");
             }
         }
     }
