b7e13d9bb7aa0945cd147a964f10ff7916cd0374
sonya <sonp2401@gmail.com>
2021-07-18 22:19:18 +0300

    266A - Камни на столе

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,39 @@ package com.company;
 import java.util.*;
 
 public class Main {
+    private static String s;
+    private static  Map<Character,Integer> arr = new HashMap<>();;
+    private static void clear(int k){
+        if (s.toCharArray()[k]=='R') {
+            arr.put('R',1);
+            arr.put('G',0);
+            arr.put('B',0);
+        }
+        if (s.toCharArray()[k]=='G') {
+            arr.put('R',0);
+            arr.put('G',1);
+            arr.put('B',0);
+        }
+        if (s.toCharArray()[k]=='B') {
+            arr.put('R',0);
+            arr.put('G',0);
+            arr.put('B',1);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        if ((int)s.toCharArray()[0]>=97) {
-            System.out.print((char)(((int)s.toCharArray()[0])-32));
-            for (int i = 1; i <s.toCharArray().length; i++) {
-                System.out.print(s.toCharArray()[i]);
+        int l=in.nextInt();
+        s = in.next();
+        int n=0;
+        //0-R, 1-G, 2-B
+        clear(0);
+        for (int i = 1; i <l; i++) {
+            if (arr.get(s.toCharArray()[i])==0){
+                clear(i);
             }
+            else n++;
         }
-        else {
-            for (int i = 0; i <s.toCharArray().length; i++) {
-                System.out.print(s.toCharArray()[i]);
-            }
-        }
+        System.out.println(n);
     }
 }
