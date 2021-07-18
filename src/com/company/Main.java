694b9c83882636380cb51bc6aaa5505f90f7d59a
sonya <sonp2401@gmail.com>
2021-07-18 21:53:39 +0300

    281A - Капитализация слова

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,31 +3,19 @@ package com.company;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n=in.nextInt(),b=in.nextInt();
-        double sum=0;
-        sum+=b;
-        int[] arr=new int[n];
-
-        for (int i = 0; i <n ; i++) {
-            arr[i]=in.nextInt();
-            sum+=arr[i];
-        }
-        boolean f=true;
-        double sum2=0;
-        for (int i = 0; i <n; i++) {
-            if (arr[i]>(sum/n)) {
-                f=false;
-                break;
+        String s = in.nextLine();
+        if ((int)s.toCharArray()[0]>=97) {
+            System.out.print((char)(((int)s.toCharArray()[0])-32));
+            for (int i = 1; i <s.toCharArray().length; i++) {
+                System.out.print(s.toCharArray()[i]);
             }
-            sum2+=(sum/n)-arr[i];
         }
-
-        if (!f||(int)sum2*1000000>(int)b*1000000) System.out.println(-1);
         else {
-            for (int i = 0; i <n ; i++) {
-                System.out.printf("%.7f\n", (sum/n)-arr[i]);
+            for (int i = 0; i <s.toCharArray().length; i++) {
+                System.out.print(s.toCharArray()[i]);
             }
         }
     }
