482d75facb8bd61c7f9ad2b9ab8c8097b2590f91
sonya <sonp2401@gmail.com>
2021-07-18 22:39:35 +0300

    236A - Девушка или Юноша

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,39 +3,16 @@ package com.company;
 import java.util.*;
 
 public class Main {
-    private static String s;
-    private static  Map<Character,Integer> arr = new HashMap<>();;
-    private static void clear(int k){
-        if (s.toCharArray()[k]=='R') {
-            arr.put('R',1);
-            arr.put('G',0);
-            arr.put('B',0);
-        }
-        if (s.toCharArray()[k]=='G') {
-            arr.put('R',0);
-            arr.put('G',1);
-            arr.put('B',0);
-        }
-        if (s.toCharArray()[k]=='B') {
-            arr.put('R',0);
-            arr.put('G',0);
-            arr.put('B',1);
-        }
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int l=in.nextInt();
-        s = in.next();
-        int n=0;
-        //0-R, 1-G, 2-B
-        clear(0);
-        for (int i = 1; i <l; i++) {
-            if (arr.get(s.toCharArray()[i])==0){
-                clear(i);
-            }
-            else n++;
+        String s=in.next();
+        Set<Character> mySet = new HashSet<>();
+
+        for (int i = 0; i <s.length() ; i++) {
+            mySet.add(s.toCharArray()[i]);
         }
-        System.out.println(n);
+
+        if (mySet.size()%2==0) System.out.println("CHAT WITH HER!");
+        else System.out.println("IGNORE HIM!");
     }
 }
