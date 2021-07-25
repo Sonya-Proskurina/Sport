bae0c8f0fe723a6f15179b9ba9277b04be43fb76
sonya <sonp2401@gmail.com>
2021-07-25 20:20:23 +0300

    59A - Слово (Kotlin)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,15 @@
-package com.company;
-
 fun main() {
-        val s: String = readLine()!!
-        var a: Int = s.split(" ").get(0).toInt()
-        var n = 0
-        while (a>=5) {
-        a-=5
-        n++
+        var s: String = readLine()!!
+        var nBig=0
+        var nLite=0
+        for (i in 0..s.length-1){
+        if (s[i].toInt()>=65&&s[i].toInt()<=90){
+        nBig++
         }
-        while (a>=4) {
-        a-=4
-        n++
+        if (s[i].toInt()>=97&&s[i].toInt()<=122){
+        nLite++
         }
-        while (a>=3) {
-        a-=3
-        n++
         }
-        while (a>=2) {
-        a-=2
-        n++
-        }
-        println(n+a)
+        s = if (nBig>nLite) s.toUpperCase() else s.toLowerCase()
+        print(s)
         }
