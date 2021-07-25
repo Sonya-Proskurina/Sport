a5f0aa5fd5735cee7971260bad9ccbef761083e5
sonya <sonp2401@gmail.com>
2021-07-25 18:33:51 +0300

    617A - Слоник (Kotlin)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,24 @@
 package com.company;
 
 fun main() {
-        var s:String = readLine()!!
-        var a:Int = s.split(" ").get(0).toInt()
-        var n:Int = s.split(" ").get(1).toInt()
-        for(i in 1..n){
-        a = if (a%10>0) --a else a/10
+        val s: String = readLine()!!
+        var a: Int = s.split(" ").get(0).toInt()
+        var n = 0
+        while (a>=5) {
+        a-=5
+        n++
         }
-        print(a)
+        while (a>=4) {
+        a-=4
+        n++
+        }
+        while (a>=3) {
+        a-=3
+        n++
+        }
+        while (a>=2) {
+        a-=2
+        n++
+        }
+        println(n+a)
         }
