1d048de14584c77f71bc89eff629da889091cfdf
sonya <sonp2401@gmail.com>
2021-07-25 18:14:14 +0300

    791A - Мишка и старший брат

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int k=in.nextInt(), money=in.nextInt(), n=in.nextInt();
-        int dolg=(k*(1+n)*n/2-money>0)?k*(1+n)*n/2-money:0;
-        System.out.println(dolg);
+        double a=in.nextInt(), b=in.nextInt();
+        int n=1;
+        while (Math.pow(1.5,n)<=b/a){
+            n++;
+        }
+        System.out.println(n);
     }
 }
