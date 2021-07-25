231436790dcf9296c8fbeb6821b02d72fea7909e
sonya <sonp2401@gmail.com>
2021-07-25 18:16:33 +0300

    977A - Неправильное вычитание (Kotlin)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,11 @@
 package com.company;
 
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        double a=in.nextInt(), b=in.nextInt();
-        int n=1;
-        while (Math.pow(1.5,n)<=b/a){
-            n++;
+fun main() {
+        var s:String = readLine()!!
+        var a:Int = s.split(" ").get(0).toInt()
+        var n:Int = s.split(" ").get(1).toInt()
+        for(i in 1..n){
+        a = if (a%10>0) --a else a/10
         }
-        System.out.println(n);
-    }
-}
+        print(a)
+        }
