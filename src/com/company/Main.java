ffa3166ff99eddba89d9ff4b8ae317853b3dff31
sonya <sonp2401@gmail.com>
2021-07-26 19:44:23 +0300

    116A - Трамвай

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,14 @@
-fun main() {
-        var s: String = readLine()!!
-        var nBig=0
-        var nLite=0
-        for (i in 0..s.length-1){
-        if (s[i].toInt()>=65&&s[i].toInt()<=90){
-        nBig++
+package com.company;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt(),n=0,k=0;
+        for (int i = 0; i <t; i++) {
+            n = n -in.nextInt() + in.nextInt();
+            if (n>k) k=n;
         }
-        if (s[i].toInt()>=97&&s[i].toInt()<=122){
-        nLite++
-        }
-        }
-        s = if (nBig>nLite) s.toUpperCase() else s.toLowerCase()
-        print(s)
-        }
+        System.out.println(k);
+    }
+}
