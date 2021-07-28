801daaaeeeeefa37d4548acaa586279baa004ab3
sonya <sonp2401@gmail.com>
2021-07-29 01:11:07 +0300

    1359B - Новая Театральная площадь

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,20 @@ public class Main {
         Scanner in =new Scanner(System.in);
         int t= in.nextInt();
         for (int i = 0; i <t; i++) {
-            long n =in.nextLong(),k=0;
-            for (long j = n/2,x=0; j >0; j--,x+=2) {
-                k+=((n-x)*4-4)*j;
+            long n=in.nextLong(), m=in.nextLong(), x=in.nextLong(), y=in.nextLong(), sum=0;
+            for (int j = 0; j <n; j++) {
+                String s=in.next();
+                for (int k = 0; k <s.length(); k++) {
+                    if (s.toCharArray()[k]=='.'&&k+1<s.length()&&s.toCharArray()[k+1]=='.'){
+                        sum=(y<2*x)?sum+y:sum+x+x;
+                        k++;
+                    }
+                    else if (s.toCharArray()[k]=='.'){
+                        sum+=x;
+                    }
+                }
             }
-            System.out.println(k);
+            System.out.println(sum);
         }
     }
 }
