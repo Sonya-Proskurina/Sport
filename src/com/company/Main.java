3cf1c839b10162a227dc114100ab27b9734f9ed8
sonya <sonp2401@gmail.com>
2021-07-28 04:21:04 +0300

    1A - Театральная площадь

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        String s=in.next();
-        for (int i = 0; i <s.length(); i++) {
-            if (s.toCharArray()[i]=='H'||s.toCharArray()[i]=='Q'||s.toCharArray()[i]=='9') {
-                System.out.println("YES");
-                break;
-            }
-            else if (i==s.length()-1) System.out.println("NO");
-        }
+        long n= in.nextLong(), m=in.nextLong(),a = in.nextLong();
+        n = (n%a==0)? n/a: n/a+1;
+        m = (m%a==0)? m/a: m/a+1;
+        System.out.println(n*m);
     }
 }
