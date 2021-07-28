5d779c54ede00700a36ed033a03cc459acb1d9d6
sonya <sonp2401@gmail.com>
2021-07-29 01:27:56 +0300

    118A - Упражнение на строки

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        int t= in.nextInt();
-        for (int i = 0; i <t; i++) {
-            long n=in.nextLong(), m=in.nextLong(), x=in.nextLong(), y=in.nextLong(), sum=0;
-            for (int j = 0; j <n; j++) {
-                String s=in.next();
-                for (int k = 0; k <s.length(); k++) {
-                    if (s.toCharArray()[k]=='.'&&k+1<s.length()&&s.toCharArray()[k+1]=='.'){
-                        sum=(y<2*x)?sum+y:sum+x+x;
-                        k++;
-                    }
-                    else if (s.toCharArray()[k]=='.'){
-                        sum+=x;
-                    }
-                }
+        String s= in.next();
+        int n=0;
+        char[] arr=new char[s.length()*2];
+        for (int i = 0; i <s.length(); i++) {
+            if (s.toCharArray()[i]!='A'&&s.toCharArray()[i]!='O'&&s.toCharArray()[i]!='Y'&&s.toCharArray()[i]!='E'&&
+                    s.toCharArray()[i]!='U'&&s.toCharArray()[i]!='I'&&
+                    s.toCharArray()[i]!='a'&&s.toCharArray()[i]!='o'&&s.toCharArray()[i]!='y'&&s.toCharArray()[i]!='e'&&
+                    s.toCharArray()[i]!='u'&&s.toCharArray()[i]!='i'){
+                arr[n]='.';
+                n++;
+                arr[n]=((int)s.toCharArray()[i]>90)?s.toCharArray()[i]:(char)((int)s.toCharArray()[i]+32);
+                n++;
             }
-            System.out.println(sum);
         }
+        System.out.println(String.valueOf(arr).replaceAll("\u0000",""));
     }
 }
