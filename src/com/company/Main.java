381adf64d7ad59a1591d475cf55ded901f705b4d
sonya <sonp2401@gmail.com>
2021-07-28 04:01:36 +0300

    110A - Почти счастливое число

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        int t=in.nextInt(),n;
-        boolean f;
-        for (int i = 0; i <t ; i++) {
-            f=false;
-            n=in.nextInt();
-            for (int j = 0; j <500; j++) {
-                for (int k = 0; k <500; k++) {
-                    if ((j+k)*2020+k==n&&(j>0||k>0)){
-                        f=true;
-                        break;
-                    }
-                    else if ((j+k)*2020+k>n) break;
-                }
-                if (f) break;
-            }
-            if (f) System.out.println("YES");
-            else System.out.println("NO");
+        long n= in.nextLong(), k=0;
+        while (n>0){
+            if (n%10==7||n%10==4) k++;
+            n/=10;
         }
+        if (k==7||k==4) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
