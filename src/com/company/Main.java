27b108a2a4f10008023d94507c3c3d24f7a490b3
sonya <sonp2401@gmail.com>
2021-07-30 22:54:18 +0300

    122A - Счастливое деление

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,48 @@
 package com.company;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
-        int n=in.nextInt(),x=0,y=0,z=0;
-        for (int i = 0; i <n; i++) {
-            x+=in.nextInt();
-            y+=in.nextInt();
-            z+=in.nextInt();
+    public static List<Integer> newHappy(int p, List<Integer> arr) {
+        List<Integer> list= new ArrayList<>();
+        for (int i = 0; i <arr.size(); i++) {
+            if (arr.get(i)>Math.pow(10,p)){
+                list.add((int)Math.pow(10,p+1)*4+arr.get(i));
+                list.add(((int)Math.pow(10,p+1)*7)+arr.get(i));
+            }
         }
-        if (x==0&&y==0&&z==0) System.out.println("YES");
+        return list;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        List<Integer> list=new ArrayList<>();
+        list.add(4);
+        list.add(7);
+        List<Integer> newA = newHappy(0,list);
+
+        for (int i = 0; i <newA.size(); i++) {
+            list.add(newA.get(i));
+        }
+        newA = newHappy(1,list);
+        for (int i = 0; i <newA.size(); i++) {
+            list.add(newA.get(i));
+        }
+
+        Collections.sort(list);
+        int ind=0,f=0;
+        while (ind<list.size()&&list.get(ind)<=n) {
+            if (n%list.get(ind)==0) {
+                f=1;
+                break;
+            }
+            ind++;
+        }
+        if (f==1) System.out.println("YES");
         else System.out.println("NO");
     }
 }
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,42 +3,15 @@ package com.company;
 import java.util.*;
 
 public class Test {
-    public static List<Integer> newHappy(int p,List<Integer> arr) {
-    List<Integer> list= new ArrayList<>();
-        for (int i = 0; i <arr.size(); i++) {
-            if (arr.get(i)>Math.pow(10,p)){
-                list.add((int)Math.pow(10,p+1)*4+arr.get(i));
-                list.add(((int)Math.pow(10,p+1)*7)+arr.get(i));
-            }
-        }
-        return list;
-    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        List<Integer> list=new ArrayList<>();
-        list.add(4);
-        list.add(7);
-        List<Integer> newA = newHappy(0,list);
-
-        for (int i = 0; i <newA.size(); i++) {
-            list.add(newA.get(i));
+        String s =in.next();
+        char[] arr= new char[s.length()];
+        for (int i = 0; i <s.length() ; i++) {
+            if ((int)s.toCharArray()[i]<=90&&i!=0) arr[i] = (char) ((int)s.toCharArray()[i]+32);
+            else if ((int)s.toCharArray()[i]>90&&i==0) arr[i] = (char) ((int)s.toCharArray()[i]-32);
+            else if ((int)s.toCharArray()[i]<=90&&i==0||(int)s.toCharArray()[i]>90&&i>0) arr[i] = (char) ((int)s.toCharArray()[i]);
         }
-        newA = newHappy(1,list);
-        for (int i = 0; i <newA.size(); i++) {
-            list.add(newA.get(i));
-        }
-
-        Collections.sort(list);
-        int ind=0,f=0;
-        while (list.get(ind)<=n) {
-            if (n%list.get(ind)==0) {
-                f=1;
-                break;
-            }
-            ind++;
-        }
-        if (f==1) System.out.println("YES");
-        else System.out.println("NO");
+        System.out.println(String.valueOf(arr));
     }
 }
