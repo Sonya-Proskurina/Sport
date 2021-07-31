ce7bcef8ee3eb23e1067f29cf5db531b335b05a9
sonya <sonp2401@gmail.com>
2021-07-31 15:50:23 +0300

    230A - Драконы

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,32 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a=in.nextInt(), b= in.nextInt(), c = in.nextInt();
-        if (a==1&&b==1&&c==1) System.out.println(3);
-        else if (a==1&&b==1) System.out.println(2*c);
-        else if (a==1&&c==1) System.out.println(b+2);
-        else if (b==1&&c==1) System.out.println(a*2);
-        else if (a==1) System.out.println((b+1)*c);
-        else if (b==1&&a<c) System.out.println((a+1)*c);
-        else if (b==1) System.out.println(a*(c+1));
-        else if (c==1) System.out.println(a*(b+1));
-        else System.out.println(a*b*c);
+        int s= in.nextInt(), n= in.nextInt();
+        int k=0;
+        boolean live = true;
+        TreeMap<Integer,Integer> arr =new TreeMap<>();
+        for (int i = 0; i <n; i++) {
+            int x=in.nextInt(), y=in.nextInt();
+            if (arr.containsKey(x)) {
+                arr.replace(x,y+arr.get(x));
+                k++;
+            }
+            else arr.put(x,y);
+        }
+        Vector<Integer> vector =new Vector(arr.keySet());
+        Vector<Integer> vector2 =new Vector(arr.values());
+        for (int i = 0; i <n-k; i++) {
+            if (s>vector.get(i)) s+=vector2.get(i);
+            else {
+                live=false;
+                break;
+            }
+        }
+        if (live) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
