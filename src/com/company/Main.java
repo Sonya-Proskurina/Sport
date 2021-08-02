0748a8109edc804af2655f998c74c61f368b2fe9
sonya <sonp2401@gmail.com>
2021-08-02 22:11:39 +0300

    C- K-е неделящееся на n (До проверки в системе)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,40 +5,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int m = in.nextInt(), d;
-        int k=0;
-        List<Integer> boys =new ArrayList<>(), girls = new ArrayList<>();
-        for (int i = 0; i <m; i++) {
-            boys.add(in.nextInt());
-        }
-        d = in.nextInt();
-        for (int i = 0; i <d; i++) {
-            girls.add(in.nextInt());
-        }
-        Collections.sort(boys);
-        Collections.sort(girls);
-        if (boys.size()>girls.size()){
-            for (int i = 0; i <girls.size(); i++) {
-                for (int j = 0; j <boys.size(); j++) {
-                    if (boys.get(j)==girls.get(i)||boys.get(j)==girls.get(i)-1||boys.get(j)==girls.get(i)+1) {
-                        boys.remove(j);
-                        k++;
-                        break;
-                    }
-                }
+        int t = in.nextInt();
+        for (int i = 0; i <t; i++) {
+            long a = in.nextLong(), b = in.nextLong(), k;
+            if (b<a) k = b;
+            else {
+                k = b+ b/(a-1);
+                if (k%a==0) k--;
             }
+            System.out.println(k);
         }
-        else {
-            for (int i = 0; i <boys.size(); i++) {
-                for (int j = 0; j <girls.size(); j++) {
-                    if (girls.get(j)==boys.get(i)||girls.get(j)==boys.get(i)-1||girls.get(j)==boys.get(i)+1) {
-                        girls.remove(j);
-                        k++;
-                        break;
-                    }
-                }
-            }
-        }
-        System.out.println(k);
     }
 }
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -5,6 +5,6 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println(5/2);
+        System.out.println((1000000000/2));
     }
 }
