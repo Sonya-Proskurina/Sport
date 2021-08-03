9bd0493f04d4dd43ade71a2d6e3dfee7516ca344
sonya <sonp2401@gmail.com>
2021-08-03 23:09:02 +0300

    Codeforces Round #734 (Div. 3) B1 - Чудесная раскраска - 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,23 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int t =in.nextInt();
         for (int i = 0; i <t ; i++) {
-            int n = in.nextInt();
-            int c2 = n/3, c1= n/3;
-            if (n%3==1) ++c1;
-            else if (n%3==2) c2++;
-            System.out.println(c1+" "+c2);
+            String s=in.next();
+            int n=0,one=0;
+            Map<Character,Integer> map = new HashMap<>();
+            for (int j = 0; j <s.length(); j++) {
+                if (map.get(s.toCharArray()[j])==null) map.put(s.toCharArray()[j],1);
+                else {
+                    map.replace(s.toCharArray()[j],map.get(s.toCharArray()[j])+1);
+                }
+            }
+            for (Integer v : map.values()) {
+                if (v==1) one++;
+                else if (v>3) {
+                    n++;
+                }
+                else n+=v/2;
+            }
+            System.out.println(n+one/2);
         }
     }
 }
