24e9ba2721d050f5790bfe30832042a641a8227c
sonya <sonp2401@gmail.com>
2021-08-03 21:58:35 +0300

    1352C - K-е неделящееся на n

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        // Прошла проверку
         int t = in.nextInt();
         for (int i = 0; i <t; i++) {
             long a = in.nextLong(), b = in.nextLong(), k;
