82488b72308830e7cdef0c45f3255593a0f4f13c
sonya <sonp2401@gmail.com>
2021-08-03 22:08:19 +0300

    Задача A. Про Гришу Н. (Примеры задач с четвертьфиналов ACM ICPC Екатеринбург, 2010-2014 годы)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // Прошла проверку
-        int t = in.nextInt();
-        for (int i = 0; i <t; i++) {
-            long a = in.nextLong(), b = in.nextLong(), k;
-            if (b<a) k = b;
-            else {
-                k = b+ b/(a-1);
-                if (k%a==0) k--;
-            }
-            System.out.println(k);
-        }
+        int f =in.nextInt();
+        if (f+5<12) System.out.println("NO");
+        else System.out.println("YES");
     }
 }
