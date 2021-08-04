fc471eff048e3df71b0412e93a0cc72102738b98
sonya <sonp2401@gmail.com>
2021-08-04 23:49:21 +0300

    734A - Антон и Даник

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String s1= in.next(), s2=in.next();
-        boolean b= s1.length()==s2.length();
-        for (int i = 0; i <s1.length(); i++) {
-            if (s1.toCharArray()[i]!=s2.toCharArray()[s2.length()-i-1]) {
-                b =false;
-            }
-            if (!b) break;
+        int n = in.nextInt(),a=0,d=0;
+        String s = in.next();
+
+        for (int i = 0; i <s.length()/2; i++) {
+            if (s.toCharArray()[i]=='A') a++;
+            else d++;
+            if (s.toCharArray()[s.length()-1-i]=='A') a++;
+            else d++;
+            if (a>n/2+1||d>n/2+1) break;
         }
-        if (b) System.out.println("YES");
-        else System.out.println("NO");
+
+        if (n-a<a) System.out.println("Anton");
+        else if (n-a>a) System.out.println("Danik");
+        else System.out.println("Friendship");
     }
 }
