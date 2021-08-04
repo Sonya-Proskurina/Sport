c9bdbc720f1fb64ca8259123399a2b2a8568ba48
sonya <sonp2401@gmail.com>
2021-08-04 23:58:09 +0300

    271A - Красивый год

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt(),a=0,d=0;
-        String s = in.next();
-
-        for (int i = 0; i <s.length()/2; i++) {
-            if (s.toCharArray()[i]=='A') a++;
-            else d++;
-            if (s.toCharArray()[s.length()-1-i]=='A') a++;
-            else d++;
-            if (a>n/2+1||d>n/2+1) break;
+        int n = in.nextInt(),k;
+        k=n+1;
+        while (k%10==k/10%10||k%10==k/100%10||k%10==k/1000
+        ||k/10%10==k/100%10||k/10%10==k/1000
+        ||k/100%10==k/1000){
+            k++;
         }
-
-        if (n-a<a) System.out.println("Anton");
-        else if (n-a>a) System.out.println("Danik");
-        else System.out.println("Friendship");
+        System.out.println(k);
     }
 }
