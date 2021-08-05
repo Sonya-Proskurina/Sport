29da4b04e9836de415e25c8391062160b06b4624
sonya <sonp2401@gmail.com>
2021-08-05 23:48:42 +0300

    1520B - Заурядные числа (медлено)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        long n = in.nextLong(),k = in.nextLong();
-        long d = (n%2==0)?n/2:n/2+1;
-        if (k<=d) System.out.println(k*2-1);
-        else System.out.println((k-d)*2);
+        long n = in.nextLong();
+        for (int i = 0; i <n; i++) {
+            int a = in.nextInt(),k=0;
+            for (int j = 1; j <=a; j++) {
+                boolean f =true;
+                int x= j,z=x%10;
+                while (x!=0){
+                    if (x%10!=z) {
+                        f=false;
+                        break;
+                    }
+                    x=x/10;
+                }
+                if (f) k++;
+            }
+            System.out.println(k);
+        }
     }
 }
