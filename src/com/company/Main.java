4b42e27de3c3ee1f9ec8f662eae7c0f2ab7d8375
sonya <sonp2401@gmail.com>
2021-08-07 16:41:11 +0300

    474B - Черви (медлено)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        long n = in.nextLong();
+        int n = in.nextInt();
+        int[] arr = new int[n];
         for (int i = 0; i <n; i++) {
-            long a = in.nextLong(),k=0;
-            for (int j = 1; j <=9; j++) {
-                long ten=0;
-                for (int l = 0; l <=9; l++) {
-                    ten+= Math.pow(10,l);
-                    if (ten*j<=a) {
-                        k++;
-                    }
-                    else break;
+            if (i==0) arr[0] = in.nextInt();
+            else arr[i] = in.nextInt() + arr[i-1];
+        }
+        int m =in.nextInt();
+        for (int i = 0; i <m; i++) {
+            int k =in.nextInt(),z=1;
+            int left=0,rigt=n,midl;
+            while (true){
+                midl=(rigt+left)/2;
+                if (arr[midl]>=k&&(midl==0||arr[midl-1]<=k)){
+                    z=midl;
+                    if (midl!=0&&arr[midl-1]==k) z--;
+                    break;
+                }
+                else if (arr[midl]>=k&&midl!=0&&arr[midl-1]>k){
+                    rigt=midl;
+                }
+                else if (arr[midl]<k){
+                    left=midl;
                 }
             }
-            System.out.println(k);
+            System.out.println(z+1);
         }
     }
 }
