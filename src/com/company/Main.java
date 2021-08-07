207f17815f4335819de98a6b5064be45055a5913
sonya <sonp2401@gmail.com>
2021-08-07 17:19:45 +0300

    677A - Ваня и забор

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,31 +5,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] arr = new int[n];
+        int n = in.nextInt(), z=in.nextInt();
+        int s=n;
         for (int i = 0; i <n; i++) {
-            if (i==0) arr[0] = in.nextInt();
-            else arr[i] = in.nextInt() + arr[i-1];
+            int a =in.nextInt();
+            if (a>z) s++;
         }
-        int m =in.nextInt();
-        for (int i = 0; i <m; i++) {
-            int k =in.nextInt(),z=1;
-            int left=0,rigt=n,midl;
-            while (true){
-                midl=(rigt+left)/2;
-                if (arr[midl]>=k&&(midl==0||arr[midl-1]<=k)){
-                    z=midl;
-                    if (midl!=0&&arr[midl-1]==k) z--;
-                    break;
-                }
-                else if (arr[midl]>=k&&midl!=0&&arr[midl-1]>k){
-                    rigt=midl;
-                }
-                else if (arr[midl]<k){
-                    left=midl;
-                }
-            }
-            System.out.println(z+1);
-        }
+        System.out.println(s);
     }
 }
