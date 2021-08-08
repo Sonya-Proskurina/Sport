1b9857cc5585a16acff6761075699f6c2de9169f
sonya <sonp2401@gmail.com>
2021-08-08 23:04:48 +0300

    467A - Юра и заселение

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt(), z=in.nextInt();
-        int s=n;
+        int n = in.nextInt(), sum =0;
         for (int i = 0; i <n; i++) {
-            int a =in.nextInt();
-            if (a>z) s++;
+            int a=in.nextInt(),b=in.nextInt();
+            if (b-a>=2) sum++;
         }
-        System.out.println(s);
+        System.out.println(sum);
     }
 }
