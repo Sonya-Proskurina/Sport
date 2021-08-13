96a685dbe56b4ec76c9ac9435f682c8fb6ed0631
sonya <sonp2401@gmail.com>
2021-08-13 23:05:45 +0300

    1363A - Выбор нечетных

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int a =in.nextInt(),ost=1;
-        for (int i = 0; i <n-1; i++) {
-            int b =in.nextInt();
-            if (b==10&&a==1||b==1&&a==10){
-                ost++;
+        for (int i = 0; i <n; i++) {
+            int len= in.nextInt(), k=in.nextInt();
+            int shet=0,neshet=0;
+            int[] arr = new int[len];
+            for (int j = 0; j <len; j++) {
+                arr[j] = in.nextInt();
+                if (arr[j]%2==0) shet++;
+                else neshet++;
             }
-            a=b;
+            boolean f =false;
+            if (k%2==0) {
+                for (int j = k-1; j >= 1; j-=2) {
+                    if (neshet>=j&&shet>=k-j) {
+                        f =true;
+                        break;
+                    }
+                }
+            }
+            else {
+                for (int j = k; j >= 1; j-=2) {
+                    if (neshet>=j&&shet>=k-j) {
+                        f =true;
+                        break;
+                    }
+                }
+            }
+            if (f) System.out.println("YES");
+            else System.out.println("NO");
         }
-        System.out.println(ost);
     }
 }
