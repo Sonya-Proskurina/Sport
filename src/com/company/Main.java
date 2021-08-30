1b1ae6d89c13728283482fcfc6add3cfea61e328
sonya <sonp2401@gmail.com>
2021-08-30 20:34:31 +0300

    1562A - The Miracle and the Sleeper

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,35 +5,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int i = 0; i <n; i++) {
-            int len= in.nextInt(), k=in.nextInt();
-            int shet=0,neshet=0;
-            int[] arr = new int[len];
-            for (int j = 0; j <len; j++) {
-                arr[j] = in.nextInt();
-                if (arr[j]%2==0) shet++;
-                else neshet++;
+        int n =in.nextInt();
+
+        for (int i = 0; i <n ; i++) {
+            int a=in.nextInt(), b = in.nextInt(), myMax = 0;
+            if (b-a<b/2) {
+                myMax = b-a;
             }
-            boolean f =false;
-            if (k%2==0) {
-                for (int j = k-1; j >= 1; j-=2) {
-                    if (neshet>=j&&shet>=k-j) {
-                        f =true;
-                        break;
-                    }
-                }
-            }
-            else {
-                for (int j = k; j >= 1; j-=2) {
-                    if (neshet>=j&&shet>=k-j) {
-                        f =true;
-                        break;
-                    }
-                }
-            }
-            if (f) System.out.println("YES");
-            else System.out.println("NO");
+            else if (b%2==0) myMax = b/2-1;
+            else  myMax = b/2;
+            System.out.println(myMax);
         }
     }
 }
