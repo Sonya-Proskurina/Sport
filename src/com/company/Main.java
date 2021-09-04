9047b0cbac1bcd72cf03413e8fb8b0225934693a
sonya <sonp2401@gmail.com>
2021-09-04 15:26:09 +0300

    337A - Пазлы

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        int n = in.nextInt(), m=in.nextInt(), min=1001;
         List<Integer> list = new ArrayList<>();
-        list.add(in.nextInt());
-        for (int i = 1; i < n; i++) {
+
+        for (int i = 0; i <m; i++) {
             list.add(in.nextInt());
-            for (int j = i - 1; j >= 0; j--) {
-                if (list.get(j) > list.get(j + 1)) {
-                    Integer r = list.get(j) - list.get(j + 1);
-                    list.set(j, list.get(j) - r);
-                    list.set(j + 1, list.get(j + 1) + r);
-                }
-            }
         }
-        for (int i = 0; i <n; i++) {
-            System.out.print(list.get(i)+" ");
+
+        Collections.sort(list);
+
+        for (int i = 0; i <=m-n; i++) {
+            if (list.get(i+n-1)-list.get(i)<min) min=list.get(i+n-1)-list.get(i);
         }
+
+        System.out.println(min);
     }
 }
