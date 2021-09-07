45c32a51c359a716c7a2e19e05030674b8bab0a5
sonya <sonp2401@gmail.com>
2021-09-07 09:36:45 +0300

    1343A - Конфеты

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        if (n >= 0) System.out.println(n);
-        else if ((n % 10 < (n / 10) % 10)) System.out.println(n / 10);
-        else if (n<=-10){
-            System.out.println((n / 100) * 10 + (n % 10));
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt(), x = 1, k = 2;
+            for (int j = 2; j <30; j++) {
+                if (n%(Math.pow(2, j)-1)==0) {
+                    x =n/(int)(Math.pow(2, j)-1);
+                    break;
+                }
+            }
+            System.out.println(x);
         }
-        else System.out.println(0);
     }
 }
