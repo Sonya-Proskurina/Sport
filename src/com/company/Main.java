025f8e1bb9152ee0f96675c1ab55ad16524cbc3b
sonya <sonp2401@gmail.com>
2021-09-08 20:42:18 +0300

    379A - Новогодние свечки

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for (int i = 0; i < t; i++) {
-            int n = in.nextInt(), x = 1, k = 2;
-            for (int j = 2; j <30; j++) {
-                if (n%(Math.pow(2, j)-1)==0) {
-                    x =n/(int)(Math.pow(2, j)-1);
-                    break;
-                }
-            }
-            System.out.println(x);
+        int t = in.nextInt(), n = in.nextInt();
+        int sum = t;
+        while (t>=n){
+            sum++;
+            t-=n;
+            t++;
         }
+        System.out.println(sum);
     }
 }
